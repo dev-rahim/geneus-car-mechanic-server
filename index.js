cef57b48a1d9052edbfe1a23b34749ccb7165fca
dev-rahim <rahimuddin5915@gmail.com>
2023-01-07 23:09:42 +0600

    post api work done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const port = 5000;
 
 app.use(cors())
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.BD_PASS}@cluster0.mzolur4.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -15,6 +16,16 @@ async function run() {
     try {
         await client.connect()
         console.log("connected to database");
+        const database = client.db("genusCarMechanic");
+        const servicesCollection = database.collection("services");
+
+        app.post('/services', async (req, res) => {
+
+            const service = req.body;
+            const result = await servicesCollection.insertOne(service)
+            res.json(result)
+        })
+
     } finally {
         // await client.close();
     }
